972b8426ec329c92abedcdd4b3c601e750331d2e
Marvin Preuss <marvin@xsteadfastx.org>
2024-09-19 14:33:36 +0200

    build: removes goreleaser

--- flake.nix
+++ flake.nix
@@ -13,6 +13,22 @@
             overlays = [ ];
           };
 
+          caddy-log-exporter = pkgs.buildGo123Module {
+            name = "caddy-log-exporter";
+            src = ./.;
+            vendorHash = null;
+            ldflags = [ "-s" "-w" "-extldflags '-static'" ];
+            CGO_ENABLED = 0;
+          };
+
+          docker = pkgs.dockerTools.buildImage {
+            name = "xsteadfastx/caddy-log-exporter";
+            tag = "latest";
+            config = {
+              Entrypoint = [ "${caddy-log-exporter}/bin/caddy-log-exporter" ];
+            };
+          };
+
           env-pkgs = with pkgs; [
             go_1_23
 
@@ -21,11 +37,17 @@
             golangci-lint
             golines
             gopls
-            goreleaser
-            ko
+
+            skopeo
           ];
         in
         with pkgs; {
+          packages =
+            {
+              default = caddy-log-exporter;
+              inherit caddy-log-exporter docker;
+            };
+
           devShells.default = mkShell {
             buildInputs = env-pkgs;
           };
